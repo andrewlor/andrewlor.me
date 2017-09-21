7146864cef5eb88fa1d95c5eb8347129d45b6b29
andrewlor <andrewlor332@gmail.com>
2017-09-21 17:36:30 -0400

    add main resume block

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Resume from './Resume'
+
+class App extends Component {
+  render() {
+    return (
+      <Resume></Resume>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div id="resume" className="centre">
+      	<h1 id="name">Andrew James Lor</h1>
+      </div>
+    );
+  }
+}
+
+export default Resume;
